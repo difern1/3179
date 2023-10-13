5903d86e1bd205fc04ce7bff8224d8e7345e0e01
difern1 <147376756+difern1@users.noreply.github.com>
2023-10-13 19:27:54 +1100

    Test upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,29 @@
+var vg_1 = "choropleth_map.json"; 
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "bar_chart.json"; 
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "bubbleplot.json"; 
+vegaEmbed("#bubbleplot", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "grouped_bar_chart.json"; 
+vegaEmbed("#groupedbar", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vg_5 = "pie_chart.json"; 
+vegaEmbed("#piechart", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
+
+var vg_6 = "scatter_plot.json"; 
+vegaEmbed("#scatterplot", vg_6).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error)
